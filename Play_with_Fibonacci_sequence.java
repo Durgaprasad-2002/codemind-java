81f384c41228e81912e92b8fffb095162429fe65
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-09-01 14:07:15 +0530

    import java.util.*; class pellseries { 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n; 		n=sc.nextInt(); 		int a=0,b=1,c; 		System.out.print(a+" "+b+" "); 		for(int i=1;i<=(n-2);i++) 		{ 			c=a+b; 			System.out.print(c+" "); 			a=b; 			b=c; 		} 	} }

---
+++ Play_with_Fibonacci_sequence.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pellseries
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		int a=0,b=1,c;
+		System.out.print(a+" "+b+" ");
+		for(int i=1;i<=(n-2);i++)
+		{
+			c=a+b;
+			System.out.print(c+" ");
+			a=b;
+			b=c;
+		}
+	}
+}
