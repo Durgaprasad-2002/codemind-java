f88be8ffb648936dca31d12bba66ca163d181e30
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-09-11 14:00:33 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a=0,b=1,c;         n=sc.nextInt();         for(int i=1;;i++)         {             c=a+b;             if (c==n)             {                 System.out.println("True");                 break;             }             else if (c>n)             {                 System.out.println("False");                 break;             }             a=b;             b=c;         }
        }
    }

---
+++ Fibonacci_or_not.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a=0,b=1,c;
+        n=sc.nextInt();
+        for(int i=1;;i++)
+        {
+            c=a+b;
+            if (c==n)
+            {
+                System.out.println("True");
+                break;
+            }
+            else if (c>n)
+            {
+                System.out.println("False");
+                break;
+            }
+            a=b;
+            b=c;
+        }
+        
+    }
+}
