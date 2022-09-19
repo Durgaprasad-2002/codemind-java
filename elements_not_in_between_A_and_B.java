e7dd9e2173cc1d88b4bfa2b385a277b2863e32a9
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-09-19 20:21:35 +0530

    import java.util.*; class notin {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a,b;         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             int c;             c=sc.nextInt();             x[i]=c;         }         a=sc.nextInt();         b=sc.nextInt();         int h=0,i1=0,i2=0;         for(int j=0;j<n;j++)         {             if (x[j]<a || x[j]>b)             {                 h=1;                 System.out.print(x[j]+" ");             }         }         if(h==0)             System.out.println(-1);     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class notin
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a,b;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int c;
+            c=sc.nextInt();
+            x[i]=c;
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int h=0,i1=0,i2=0;
+        for(int j=0;j<n;j++)
+        {
+            if (x[j]<a || x[j]>b)
+            {
+                h=1;
+                System.out.print(x[j]+" ");
+            }
+        }
+        if(h==0)
+            System.out.println(-1);
+    }
+}
